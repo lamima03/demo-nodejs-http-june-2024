d2ed8ea6c74a8d94820ff3338b23402d67eeb930
Francois Disubi <francois@kinshasadigital.com>
2024-06-04 11:43:18 +0100

    feat: express

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-const http = require("http");
-const fs = require("fs")
-
-const server = http.createServer(function (req, res) {
-  if (req.url === "/") {
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.end(fs.readFileSync("./index.html", "utf-8"))
-  } else if (req.url === "/contact") {
-    // retourner la page contact
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.end(fs.readFileSync("./contact.html", "utf-8"))
-  } else if(req.url === "/about"){
-    // retourner la page about
-  } else {
-        // retourner la page 404
-  }
-});
+// const http = require("http");
+// const fs = require("fs")
+const path = require("path")
 
 
-const port = 3000
+const express = require("express");
 
-server.listen(port, function(){
-    console.log("Le serveur est lancé")
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./index.html"))
 })
 
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve("./contact.html"))
+})
 
+// const server = http.createServer(function (req, res) {
+//   if (req.url === "/") {
+//     res.writeHead(200, {"Content-Type": "text/html"})
+//     res.end(fs.readFileSync("./index.html", "utf-8"))
+//   } else if (req.url === "/contact") {
+//     // retourner la page contact
+//     res.writeHead(200, {"Content-Type": "text/html"})
+//     res.end(fs.readFileSync("./contact.html", "utf-8"))
+//   } else if(req.url === "/about"){
+//     // retourner la page about
+//   } else {
+//         // retourner la page 404
+//   }
+// });
 
+const port = 3001;
+
+// server.listen(port, function(){
+//     console.log("Le serveur est lancé")
+// })
+
+app.listen(port, function () {
+  console.log("Le serveur est lancé");
+});

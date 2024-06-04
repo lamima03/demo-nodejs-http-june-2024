86a2ad42b1633faa3146c57b3393d809332af87e
ChrisMwanya <cmwanya@gmail.com>
2024-06-04 10:04:10 +0100

    Add 404, about, and contact pages

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const http = require("http");
-const fs = require("fs")
+const fs = require("fs");
 
 const server = http.createServer(function (req, res) {
   if (req.url === "/") {
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.end(fs.readFileSync("./index.html", "utf-8"))
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./index.html", "utf-8"));
   } else if (req.url === "/contact") {
     // retourner la page contact
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.end(fs.readFileSync("./contact.html", "utf-8"))
-  } else if(req.url === "/about"){
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./contact.html", "utf-8"));
+  } else if (req.url === "/about") {
     // retourner la page about
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./about.html", "utf-8"));
   } else {
-        // retourner la page 404
+    // retourner la page 404
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./404.html", "utf-8"));
   }
 });
 
+const port = 3000;
 
-const port = 3000
-
-server.listen(port, function(){
-    console.log("Le serveur est lancé")
-})
-
-
-
+server.listen(port, function () {
+  console.log("Le serveur est lancé");
+});
